3dbeac0c21b3b8021105489913d6d74ee21eab14
Pranaybaharghare <pranaybaharghare27@gmail.com>
2024-04-19 17:06:56 +0530

    cloudinary wise code commit

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,23 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinaary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null;
+
+        const response = await cloudinary.uploader.upload(localFilePath,
+            { resource_type: "auto" })
+        console.log("File upload successfully", response);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath); //remove locally saved temp file from the server if it failed to upload on cloudinary
+    }
+
+}
+export default uploadOnCloudinaary;
